dfde7a5e95398e20901e1277fbdcd7ea8892b213
ElenaNizh <lena-vn@yandex.ru>
2022-09-19 14:07:12 +0300

    коммит в ветку task2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,53 +1,47 @@
 public class Main {
+    static int ALL_LIMIT = 10;
 
-    static int ALL_LIMIT = 10; // салон должен продать максимум 10 авто
+    public static void main(String[] args) {
 
-    public static void main(String[] args) throws InterruptedException {
         final ProducerConsumer produceConsume = new ProducerConsumer();
-        final int sleepTimeForThreads = 3_500;
+        final int sleepTimeProduceConsume = 500;
+        //static int ALL_LIMIT = 10;
+
 
         ThreadGroup group = new ThreadGroup("Группа");
 
         new Thread(group, () -> {
             try {
-                produceConsume.produceCar();
+                produceConsume.consume();
+            } catch (InterruptedException e) {
+                Thread.currentThread().isInterrupted();
+            }
+        }, "Покупатель1").start();
+
+        new Thread(group, () -> {
+            try {
+                produceConsume.consume();
+            } catch (InterruptedException e) {
+                Thread.currentThread().isInterrupted();
+            }
+        }, "Покупатель2").start();
+
+        new Thread(group, () -> {
+            try {
+                produceConsume.consume();
+            } catch (InterruptedException e) {
+                Thread.currentThread().isInterrupted();
+            }
+        }, "Покупатель3").start();
+
+        new Thread(group, () -> {
+            try {
+                Thread.sleep(sleepTimeProduceConsume);
+                produceConsume.produce();
             } catch (InterruptedException e) {
                 Thread.currentThread().isInterrupted();
             }
         }, "Toyota").start();
-
-        // Thread.sleep(sleepTimeForThreads);
-
-        while (produceConsume.numberOfSoldCars < ALL_LIMIT) {
-            new Thread(group, () -> {
-                try {
-                    produceConsume.consumeCar();
-                } catch (InterruptedException e) {
-                    Thread.currentThread().isInterrupted();
-                }
-            }, "Покупатель1").start();
-
-            Thread.sleep(sleepTimeForThreads);
-
-            new Thread(group, () -> {
-                try {
-                    produceConsume.consumeCar();
-                } catch (InterruptedException e) {
-                    Thread.currentThread().isInterrupted();
-                }
-            }, "Покупатель2").start();
-
-            Thread.sleep(sleepTimeForThreads);
-
-            new Thread(group, () -> {
-                try {
-                    produceConsume.consumeCar();
-                } catch (InterruptedException e) {
-                    Thread.currentThread().isInterrupted();
-                }
-            }, "Покупатель3").start();
-            //Thread.sleep(sleepTimeForThreads);
-        }
-        group.interrupt();
     }
 }
+
--- src/main/java/ProducerConsumer.java
+++ src/main/java/ProducerConsumer.java
@@ -1,49 +1,66 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class ProducerConsumer {
 
+    private static final Lock lock = new ReentrantLock(true);
+    private final Condition conditionMake = lock.newCondition(); // ожидаемое событие - изготовление авто
+    private final Condition conditionBuy = lock.newCondition(); // ожидаемое событие - покупка машины
+
     private List<Car> carForSale = new ArrayList<>();
-    private final int LIMIT = 1; //  за один раз выпускается 1 автомобиль
-
-    // Время на изготовление одного автомобиля
-    private final int sleepTimeForProduce = 1500;
-
+    // за один раз выпускается 1 автомобиль
+    private final int LIMIT = 1;
+    private final int sleepTimeForProduce = 1_500;
     private final int sleepTimeForConsume = 500;
-
-    Car car = new Car("Toyota");
     int numberOfSoldCars = 0;
 
-    public synchronized void produceCar() throws InterruptedException {
-        while (carForSale.size() < Main.ALL_LIMIT) {
-            Thread.sleep(sleepTimeForProduce);
-            carForSale.add(car);
-            System.out.println("Производитель " + Thread.currentThread().getName() + " выпустил " + carForSale.size() + " авто");
-            wait(sleepTimeForConsume);
-            notify();
+    public void produce() throws InterruptedException {
+        try {
+            lock.lock();
+            // while (numberOfSoldCars < 10) {
+            while (numberOfSoldCars < Main.ALL_LIMIT) {
+                // while (carForSale.size() < Main.ALL_LIMIT) {
+                while (carForSale.size() == LIMIT) {
+                    conditionBuy.await();
+                }
+                Thread.sleep(sleepTimeForProduce);
+                carForSale.add(new Car("Toyota"));
+                System.out.println("Производитель " + Thread.currentThread().getName() + " выпустил " + carForSale.size() + " авто");
+                conditionMake.signal();
+            }
+        } finally {
+            lock.unlock();
         }
-        wait();
     }
 
-    public synchronized void consumeCar() throws InterruptedException {
-        Thread.sleep(sleepTimeForConsume);
-        System.out.println(Thread.currentThread().getName() + " зашел в автосалон");
+    public void consume() throws InterruptedException {
+        while (carForSale.size() < Main.ALL_LIMIT) {
+            try {
+                lock.lock();
+                Thread.sleep(sleepTimeForConsume);
+                System.out.println(Thread.currentThread().getName() + " вошел в салон");
+                if (carForSale.size() == 0) {
+                    System.out.println("Машины нет");
+                }
+                conditionMake.await();
 
-        // Если машин нет, покупатель ждёт поступления автомобиля
-        if (carForSale.size() == 0) {
-            System.out.println("Машин нет");
-            wait();
-        }
-        Thread.sleep(sleepTimeForConsume);
-        numberOfSoldCars++;
-        System.out.println(Thread.currentThread().getName() + " уехал на новеньком авто");
-        System.out.println("Куплено " + numberOfSoldCars + " авто");
+                Thread.sleep(sleepTimeForConsume);
+                numberOfSoldCars++;
 
-        try {
-            carForSale.remove(0);
-            //  carForSale.remove(carForSale.size()-carForSale.size());
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("Внимание, машин больше не будет");
+                System.out.print(Thread.currentThread().getName() + " уехал на новеньком авто. ");
+                System.out.println("Куплено " + numberOfSoldCars + " авто");
+                if (carForSale.size() == 0) {
+                    System.out.println("Машин нет");
+                }
+                System.out.println("Машин нет, покупатели ждут выпуска");
+                carForSale.remove(0);
+                conditionBuy.signal();
+            } finally {
+                lock.unlock();
+            }
         }
     }
 }
